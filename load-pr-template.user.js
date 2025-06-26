5325100eb181d22c1caaca3a8380935fd7e62f36
garanmoon <garan@dable.io>
2025-06-26 12:42:26 +0900

    require 버저닝

--- load-pr-template.user.js
+++ load-pr-template.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Load PR Template
 // @namespace    http://tampermonkey.net/
-// @version      25062501
+// @version      25062601
 // @description  PR Template 내용을 가져옵니다.
 // @author       garan-dable
 // @match        https://github.com/teamdable/*
-// @updateURL    https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template@latest/load-pr-template.user.js
-// @downloadURL  https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template@latest/load-pr-template.user.js
-// @require      https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template/templates.js
-// @require      https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template/main.js
+// @updateURL    https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template@25062601/load-pr-template.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template@25062601/load-pr-template.user.js
+// @require      https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template@25062601/templates.js
+// @require      https://cdn.jsdelivr.net/gh/garan-dable/load-pr-template@25062601/main.js
 // @grant        none
 // ==/UserScript==
